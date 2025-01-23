e0ac6d3af216e2f6727e0ba41db430535dcf63f9
CodeAssasinKing <bayramowramazan85@gmail.com>
2025-01-23 18:37:34 +0500

    added languages

--- turkmenali/settings.py
+++ turkmenali/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -112,6 +113,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+LANGUAGES = [
+    ("en", "English"),
+    ("tr", "Turkish")
+]
 
 
 # Static files (CSS, JavaScript, Images)
